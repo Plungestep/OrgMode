a70a7e2c8f004882341ba58c1d5b7d1a1faa5cbf
Kris <7462063+Plungestep@users.noreply.github.com>
2024-12-10 15:48:20 -0600

    Update .custom.el
    I used AI to clean up my custom.el file.

--- .custom.el
+++ .custom.el
@@ -1,33 +1,31 @@
-;;
-;; Org Mode
-;;
-(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+;;; Org Mode Configuration
+
+;; Basic Setup
 (require 'org)
-;;
-;; Hide italics and bold markers
-(setq org-hide-emphasis-markers t)
-;;
-;; Make bullets look nicer
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+
+;; Visual Enhancements
+(setq org-hide-emphasis-markers t)  ; Hide markup symbols for bold, italic, etc.
+
+;; Replace list markers with bullets
 (font-lock-add-keywords 'org-mode
-                        '(("^ +\\([-*]\\) "
-                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-;;
-;; Standard key bindings
+                       '(("^ +\\([-*]\\) "
+                          (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+;; Key Bindings
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
-;;
-;; Custom Key Bindings
-(global-set-key (kbd "<f4>") 'org-agenda)
+(global-set-key (kbd "<f4>")  #'org-agenda)  ; Redundant, but might be useful
 
+;; TODO Keywords Configuration
 (setq org-todo-keywords
-      (quote ((sequence "STARTED(d)" "TODO(t)" "WAITING(w)" "SOMEDAY(s)" "|" "DONE(d)" "CANCELLED(c)")
-              )))
+      '((sequence "STARTED(d)" "TODO(t)" "WAITING(w)" "SOMEDAY(s)" "|" "DONE(d)" "CANCELLED(c)")))
 
 (setq org-todo-keyword-faces
-      (quote (("TODO" :foreground "red" :weight bold)
-              ("STARTED" :foreground "blue" :weight bold)
-              ("DONE" :foreground "forest green" :weight bold)
-              ("WAITING" :foreground "orange" :weight bold)
-              ("SOMEDAY" :foreground "magenta" :weight bold)
-              ("CANCELLED" :foreground "forest green" :weight bold))))
+      '(("TODO"      :foreground "red"          :weight bold)
+        ("STARTED"   :foreground "blue"         :weight bold)
+        ("DONE"      :foreground "forest green" :weight bold)
+        ("WAITING"   :foreground "orange"       :weight bold)
+        ("SOMEDAY"   :foreground "magenta"      :weight bold)
+        ("CANCELLED" :foreground "forest green" :weight bold)))
