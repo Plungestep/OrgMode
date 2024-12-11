4a1e4882eeb0df254c8f65123493583372b00414
Kris <7462063+Plungestep@users.noreply.github.com>
2024-12-11 17:57:38 -0600

    Update .custom.el

--- .custom.el
+++ .custom.el
@@ -39,12 +39,6 @@
 ; Use the current window for indirect buffer display
 (setq org-indirect-buffer-display 'current-window)
 
-;;;; Refile settings
-; Exclude DONE state tasks from refile targets
-(defun bh/verify-refile-target ()
-  "Exclude todo keywords with a done state from refile targets"
-  (not (member (nth 2 (org-heading-components)) org-done-keywords)))
-
 (setq org-refile-target-verify-function 'bh/verify-refile-target)
 
 ;; Enable global auto-revert
