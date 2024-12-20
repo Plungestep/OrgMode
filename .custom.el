a58b0c7c43a19fe73f46d365b2e602257c08e29e
Kris <7462063+Plungestep@users.noreply.github.com>
2024-12-20 10:58:39 -0600

    Update .custom.el
    Added dashboard.

--- .custom.el
+++ .custom.el
@@ -34,6 +34,27 @@
 ;; Tell emacs to put backups in the trash directory
 (setq backup-directory-alist '((".*" . "~/Nextcloud/org/Trash")))
 
+;; Start with dashboard
+(use-package dashboard
+  :ensure t 
+  :init
+  (setq initial-buffer-choice 'dashboard-open)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")
+  ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
+  (setq dashboard-startup-banner "~/.config/emacs/images/dtmacs-logo.png")  ;; use custom image as banner
+  (setq dashboard-center-content nil) ;; set to 't' for centered content
+  (setq dashboard-items '((recents . 5)
+                          (agenda . 5 )
+                          (bookmarks . 3)
+                          (projects . 3)))
+  :custom 
+  (dashboard-modify-heading-icons '((recents . "file-text")
+				      (bookmarks . "book")))
+  :config
+  (dashboard-setup-startup-hook))
+
 ;; Setup use-package first
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
