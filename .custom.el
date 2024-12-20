acb1fe4d944f23f18712c3ab23065d572618fd75
Kris <7462063+Plungestep@users.noreply.github.com>
2024-12-20 10:49:29 -0600

    Update .custom.el
    Tried to improve the look and usability with some recommended changes.

--- .custom.el
+++ .custom.el
@@ -6,6 +6,34 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(use-package rainbow-delimiters
+  :hook ((emacs-lisp-mode . rainbow-delimiters-mode)
+         (clojure-mode . rainbow-delimiters-mode)))
+
+;; Sane default settings
+(delete-selection-mode 1)    ;; You can select text and delete it by typing.
+(electric-indent-mode -1)    ;; Turn off the weird indenting that Emacs does by default.
+(electric-pair-mode 1)       ;; Turns on automatic parens pairing
+;; The following prevents <> from auto-pairing when electric-pair-mode is on.
+;; Otherwise, org-tempo is broken when you try to <s TAB...
+(add-hook 'org-mode-hook (lambda ()
+           (setq-local electric-pair-inhibit-predicate
+                   `(lambda (c)
+                  (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+(global-auto-revert-mode t)  ;; Automatically show changes if the file has changed
+(global-display-line-numbers-mode 1) ;; Display line numbers
+(global-visual-line-mode t)  ;; Enable truncated lines
+(menu-bar-mode -1)           ;; Disable the menu bar 
+(scroll-bar-mode -1)         ;; Disable the scroll bar
+(tool-bar-mode -1)           ;; Disable the tool bar
+(setq org-edit-src-content-indentation 0) ;; Set src block automatic indent to 0 instead of 2.
+(setq use-file-dialog nil)   ;; No file dialog
+(setq use-dialog-box nil)    ;; No dialog box
+(setq pop-up-windows nil)    ;; No popup windows
+
+;; Tell emacs to put backups in the trash directory
+(setq backup-directory-alist '((".*" . "~/Nextcloud/org/Trash")))
+
 ;; Setup use-package first
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
